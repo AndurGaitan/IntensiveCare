5fdbd1c4d7a73761e926682868571ab5c36eadc2
AndurGaitan <andresdavidagaitan@hotmail.com>
2024-08-31 10:44:21 -0300

    Initial commit: Configuración básica de App Intensive Care

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+
+
+const NavBar = () => {
+  return (
+    <nav className="bg-green-500 p-4">
+      <h1 className="text-white text-2xl">IntensiveCare</h1>
+    </nav>
+  )
+}
+
+export default NavBar
